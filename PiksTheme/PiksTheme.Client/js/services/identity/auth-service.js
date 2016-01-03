c9e621f42652ef4dea131eb03a93f603460116f1
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2016-01-03 22:39:14 +0200

    Adding authentication base

---
+++ PiksTheme/PiksTheme.Client/js/services/identity/auth-service.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    var authService = function authService(identity, data) {
+        function register(user) {
+
+        }
+
+        function login(user) {
+
+        }
+
+        function logout() {
+
+        }
+
+        return {
+            register: register,
+            login: login,
+            logout: logout
+        };
+    };
+
+    angular.module('piksTheme.services')
+        .factory('auth', ['identity', 'data', authService]);
+}());
---
+++ PiksTheme/PiksTheme.Client/js/services/identity/identity-service.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    function identity($cookies) {
+        function getCurrentUser() {
+
+        }
+
+        function setCurrentUser(user) {
+
+        }
+
+        function getIdentity() {
+
+        }
+
+        function isAuthenticated() {
+
+        }
+
+        return {
+            getCurrentUser: getCurrentUser,
+            setCurrentUser: setCurrentUser,
+            getIdentity: getIdentity,
+            isAuthenticated: isAuthenticated
+        }
+    }
+
+    angular.module('piksTheme.services')
+        .factory('identity', ['$cookies', identity]);
+}());
