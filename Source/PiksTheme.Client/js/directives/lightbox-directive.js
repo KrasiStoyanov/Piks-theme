c1380698a5f860a8880cfedf2095d30721868eae
KrasiStoyanov <kradi_st@abv.bg>
2016-01-04 15:52:51 +0200

    Made the open lightbox js code work, but should be refactored..

---
+++ Source/PiksTheme.Client/js/directives/lightbox-directive.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    function lightbox() {
+        return {
+            restrict: 'A',
+            templateUrl: 'views/directives/lightbox-directive.html',
+            link: function () {
+                function openCategoryDetailsjQuery() {
+                    debugger;
+                    $('#lightbox')
+                        .css('top', $(window).scrollTop())
+                        .addClass('-active');
+
+                    $('#lightbox').offset().top == $('#lightbox').css('top');
+
+                    setTimeout(function () {
+                        $('body').addClass('-category-view-opened');
+                    }, 1);
+                }
+
+                function closeCategoryDetailsjQuery() {
+                    $('#lightbox')
+                        .removeClass('-active');
+
+                    setTimeout(function () {
+                        $('#lightbox').css('top', '');
+                    }, 700);
+                }
+                
+                $('.lightbox-link').on('click', openCategoryDetailsjQuery);
+                $('.-close-view').on('click', closeCategoryDetailsjQuery);
+            }
+        }
+    }
+
+    angular.module('piksTheme.directives')
+        .directive('lightbox', [lightbox]);
+}());
